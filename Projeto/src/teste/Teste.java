7a346a3b13e5e8d8b35f441f52845e1415e045da
Lenon <razorraver1@hotmail.com>
2020-09-11 14:21:57 -0300

    Alterado pelo Eclipse.

--- Projeto/src/teste/Teste.java
+++ Projeto/src/teste/Teste.java
@@ -5,5 +5,6 @@ public class Teste {
 	public static void main(String[] args) {
 		System.out.println("Teste");
 		System.out.println("Alterado do github.");
+		System.out.println("Alterado pelo Eclipse.");
 	}
 }
